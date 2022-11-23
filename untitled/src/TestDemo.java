b4b220a28eba2d865cbf9da66df3cb60a05d6de8
浮中有夸 <fzyk.dylj@foxmail.com>
2022-11-23 17:38:10 +0800

    hello

---
+++ untitled/src/TestDemo.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: fzykd
+ * Date: 2022-11-23
+ * Time: 17:37
+ */
+
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
