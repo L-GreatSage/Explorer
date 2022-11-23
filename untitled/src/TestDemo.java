c241876e114438c235dcd73a754300dba5e76540
L-GreatSage <118816980+L-GreatSage@users.noreply.github.com>
2022-11-23 18:53:12 +0800

    Update TestDemo.java

--- untitled/src/TestDemo.java
+++ untitled/src/TestDemo.java
@@ -10,5 +10,14 @@ public class TestDemo {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        
     }
 }
